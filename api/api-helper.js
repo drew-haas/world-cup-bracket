7e2e7960daf430bc11c49b68e28f9d44da0246b5
Andrew Haas <ahaas3@apple.com>
2022-01-29 09:34:51 -0800

    add default to api function

--- api/api-helper.js
+++ api/api-helper.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 const API_ENDPOINT = "https://livescore-api.com/api-client/competitions/list.json&key=2XfATHINJMuaC9JV&secret=utjPmUCIvDOFhRPjP5UIMDvwp0G29qXE"
 
-export async function handler(event, context) {
+export default async function handler(event, context) {
 	try {
 		const response = await axios.get(API_ENDPOINT, { headers: { Accept: "application/json" } })
 		const data = response.data
